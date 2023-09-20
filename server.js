99193bae9f0ad39cd0bc55cc34146a3a5ca62b03
Cristian <cristian.rr99@gmail.com>
2023-09-20 15:32:02 -0300

    commit inicial

---
+++ server.js
@@ -0,0 +1,16 @@
+const jsonServer = require("json-server");
+
+const server = jsonServer.create();
+
+const router = jsonServer.router("/api", "db.json");
+
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+const porta = process.env.PORT | 3000;
+
+server.listen(porta, () => {
+  console.log(`A API do NoteKeeper está rodando na porta: ${porta}`);
+});
