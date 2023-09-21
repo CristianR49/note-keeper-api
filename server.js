3a7820ede9a1a0c04c422bbd5d3e4dc9ac0f044a
Cristian <cristian.rr99@gmail.com>
2023-09-21 14:35:06 -0300

    alteração

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const jsonServer = require("json-server");
 
 const server = jsonServer.create();
 
-const router = jsonServer.router("/api", "db.json");
+const router = jsonServer.router("db.json");
 
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(router);
+server.use("/api", router);
 
 const porta = process.env.PORT | 3000;
 
